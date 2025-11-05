492c927b99198013b8a491852f246ce1141f4804
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-05 04:02:28 +0200

    add text fields to bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app_exmp/widgets/custom_bottom.dart';
 import 'package:notes_app_exmp/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,15 +9,15 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Container(
-        child: Column(
-          children: [
-            SizedBox(height: 20,),
-            CustomTextField(hintTxt: 'Title'),
-            SizedBox(height: 15,),
-            CustomTextField(hintTxt: 'Content', maxLines: 5,)
-          ],
-        ),
+      child: Column(
+        children: [
+          SizedBox(height: 20,),
+          CustomTextField(hintTxt: 'Title'),
+          SizedBox(height: 15,),
+          CustomTextField(hintTxt: 'Content', maxLines: 5,),
+          SizedBox(height: 100,),
+          CustomButton(txt: 'Add', onTap: (){}),
+        ],
       ),
     );
   }
---
+++ lib/widgets/custom_bottom.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  CustomButton({super.key, required this.txt , required this.onTap});
+  final String txt;
+  VoidCallback onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Color(0xFF60EEE2),
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Text(
+          txt,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w500,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
