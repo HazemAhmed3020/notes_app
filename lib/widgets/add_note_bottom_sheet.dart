b495c6da8adf5565666bc7df105d74faa219ab3d
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-05 04:11:54 +0200

    finish bottom sheet

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -9,15 +9,19 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Column(
+      child: ListView(
         children: [
-          SizedBox(height: 20,),
-          CustomTextField(hintTxt: 'Title'),
-          SizedBox(height: 15,),
-          CustomTextField(hintTxt: 'Content', maxLines: 5,),
-          SizedBox(height: 100,),
-          CustomButton(txt: 'Add', onTap: (){}),
-        ],
+          Column(
+          children: [
+            SizedBox(height: 20,),
+            CustomTextField(hintTxt: 'Title'),
+            SizedBox(height: 15,),
+            CustomTextField(hintTxt: 'Content', maxLines: 5,),
+            SizedBox(height: 100,),
+            CustomButton(txt: 'Add', onTap: (){}),
+          ],
+        ),
+    ],
       ),
     );
   }
