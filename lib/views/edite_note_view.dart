1a25e103687d85f5be23d3f2803695c10f7037d1
Hazem Ahmed <hazemahmedsaed@gmail.com>
2025-11-10 02:10:48 +0200

    finish edit note

--- lib/views/edite_note_view.dart
+++ lib/views/edite_note_view.dart
@@ -2,9 +2,16 @@
 import 'package:notes_app_exmp/widgets/custom_text_field.dart';
 
 import '../widgets/custom_appbar.dart';
- class EditeNoteView extends StatelessWidget {
+ class EditeNoteView extends StatefulWidget {
    const EditeNoteView({super.key});
 
+  @override
+  State<EditeNoteView> createState() => _EditeNoteViewState();
+}
+
+class _EditeNoteViewState extends State<EditeNoteView> {
+   String? title;
+   String? content;
    @override
    Widget build(BuildContext context) {
      return Scaffold(
@@ -15,13 +22,20 @@ import '../widgets/custom_appbar.dart';
              SizedBox(height: 35),
              CustomAppbar(icon: Icons.check, title: 'Edit Note',),
              SizedBox(height: 30,),
-             CustomTextField(hintTxt: 'Title'),
+             CustomTextField(hintTxt: 'Title' , onChanged: (data){
+               title = data;
+             },
+             ),
              SizedBox(height: 30,),
-             CustomTextField(hintTxt: 'content', maxLines: 7,),
+             CustomTextField(hintTxt: 'content', maxLines: 7,
+             onChanged: (data){
+               content = data;
+             },
+             ),
            ],
          ),
 
        ),
      );
    }
- }
+}
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
- const CustomTextField({super.key, required this.hintTxt, this.maxLines = 1, this.onSaved,});
+ const CustomTextField({super.key, required this.hintTxt, this.maxLines = 1, this.onSaved, this.onChanged,});
   final String hintTxt;
   final int maxLines;
  final void Function(String?)? onSaved;
-
+ final void Function(String)? onChanged;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -18,6 +18,7 @@ class CustomTextField extends StatelessWidget {
           return null;
         }
       },
+      onChanged: onChanged,
       onSaved: onSaved,
       maxLines: maxLines,
       decoration: InputDecoration(
